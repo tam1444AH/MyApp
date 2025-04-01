fa219f8b78fbc6bb21a38e8234f8dc95acbc23df
tam1444AH <mohammedtalha290@gmail.com>
2025-03-31 20:57:35 -0500

    mvc app

--- MyApp/Controllers/ItemsController.cs
+++ MyApp/Controllers/ItemsController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyApp.Data;
 using MyApp.Models;
 
 namespace MyApp.Controllers
@@ -8,6 +10,40 @@ namespace MyApp.Controllers
         public IActionResult Overview()
         {
             var item = new Item() { Name = "keyboard" };
+            return View(item);
+        }
+
+        public IActionResult Edit(int itemId)
+        {
+            return Content("itemId = " + itemId);
+        }
+
+        private readonly MyAppContext _context;
+        public ItemsController(MyAppContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var item = await _context.Items.ToListAsync();
+            return View(item);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("Id, Name, Price")] Item item)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Items.Add(item);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
             return View(item);
         }
     }
---
+++ MyApp/Data/MyAppContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyApp.Models;
+
+namespace MyApp.Data
+{
+    public class MyAppContext : DbContext
+    {
+        public MyAppContext(DbContextOptions<MyAppContext> options) : base(options) { }
+        
+        public DbSet<Item> Items { get; set; }
+    }
+}
